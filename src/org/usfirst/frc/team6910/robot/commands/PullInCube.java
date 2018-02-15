2016b8d559f77d7bb67d70abacf6884227158379
CheesecakeCG <cheesecakecgcontact@gmail.com>
2018-02-15 16:09:29 -0500

    oops

---
+++ src/org/usfirst/frc/team6910/robot/commands/PullInCube.java
@@ -0,0 +1,52 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package org.usfirst.frc.team6910.robot.commands;
+
+import edu.wpi.first.wpilibj.command.Command;
+import org.usfirst.frc.team6910.robot.Robot;
+
+/**
+ * An example command.  You can replace me with your own command.
+ */
+public class PullInCube extends Command {
+	public PullInCube() {
+		// Use requires() here to declare subsystem dependencies
+		requires(Robot.m_cc);
+	}
+
+	// Called just before this Command runs the first time
+	@Override
+	protected void initialize() {
+		
+	}
+
+	// Called repeatedly when this Command is scheduled to run
+	@Override
+	protected void execute() {
+		Robot.m_cc.pull();
+	}
+
+	// Make this return true when this Command no longer needs to run execute()
+	@Override
+	protected boolean isFinished() {
+		Robot.m_cc.stop();
+		return true;
+	}
+
+	// Called once after isFinished returns true
+	@Override
+	protected void end() {
+	}
+
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	@Override
+	protected void interrupted() {
+		Robot.m_cc.stop();
+	}
+}
---
+++ src/org/usfirst/frc/team6910/robot/subsystems/TankDrive.java
@@ -0,0 +1,51 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package org.usfirst.frc.team6910.robot.subsystems;
+
+import org.usfirst.frc.team6910.robot.Robot;
+
+import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+/**
+ * An example subsystem.  You can replace me with your own Subsystem.
+ */
+public class TankDrive extends Subsystem {
+	// Put methods for controlling this subsystem
+	// here. Call these from Commands.
+	private DifferentialDrive m_DiffDrive; // Differential Drive for controlling drive chain 
+	private double m_MotorSensitivity = -.8f;
+	
+	public void initDefaultCommand() {
+		// Set the default command for a subsystem here.
+		// setDefaultCommand(new MySpecialCommand());
+		Spark m_leftFrontSpark = new Spark(Robot.m_robotMap.leftFrontMotor);
+		Spark m_leftBackSpark = new Spark(Robot.m_robotMap.leftBackMotor);
+		SpeedControllerGroup m_leftMotorGroup = new SpeedControllerGroup(m_leftFrontSpark, m_leftBackSpark);
+		
+		Spark m_rightFrontSpark = new Spark(Robot.m_robotMap.rightFrontMotor);
+		Spark m_rightBackSpark = new Spark(Robot.m_robotMap.rightBackMotor);
+		SpeedControllerGroup m_rightMotorGroup = new SpeedControllerGroup(m_rightFrontSpark, m_rightBackSpark);
+		
+		m_DiffDrive = new DifferentialDrive(m_leftMotorGroup, m_rightMotorGroup);
+		
+	}
+	
+	public void drive(double left, double right) {
+		m_DiffDrive.tankDrive( left * m_MotorSensitivity, right * m_MotorSensitivity);
+	}
+	
+	public void set_sens(double s) {
+		m_MotorSensitivity = s;
+	}
+	public double get_sens(double s) {
+		return s;
+	}
+}
