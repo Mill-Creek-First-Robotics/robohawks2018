f1c15cc59b523d4cc032b86ce00f4f37c8d817f4
CheesecakeCG <cheesecakecgcontact@gmail.com>
2018-03-24 09:07:23 -0400

    Added fancy side auto; Now places cubes in our switch.

--- src/org/usfirst/frc/team6910/robot/commands/auto/FancyAuto.java
+++ src/org/usfirst/frc/team6910/robot/commands/auto/FancyAuto.java
@@ -10,29 +10,49 @@ public class FancyAuto extends CommandGroup {
 	private char position = 'c';
 	private char target;
 	boolean safeToPlace = true;
+	private long length;
+	private boolean direction = false;
 	
 	public FancyAuto() {
 		centerLength = Robot.m_prefs.getLong("Center Length", 500);
 		position = Robot.m_prefs.getString("Position", "c").charAt(0);
-		long[] timings =  {Robot.m_prefs.getLong("centerAutoTimingA", 1000), Robot.m_prefs.getLong("centerAutoTimingB", 1000), Robot.m_prefs.getLong("centerAutoTimingC", 1000)}; 
+		long[] timings =  {Robot.m_prefs.getLong("centerAutoTimingA", 500), Robot.m_prefs.getLong("centerAutoTimingB", 750), Robot.m_prefs.getLong("centerAutoTimingC", 250)};
+		length = Robot.m_prefs.getLong("SidesAutoLength", 2000);
 		
 		DriverStation.reportWarning(timings.toString(), false);
 
 		String gameData = Robot.m_ds.getGameSpecificMessage();
-		
 		
 		if (gameData.length() > 0){
 			target = gameData.charAt(0);
 		} else {
 			safeToPlace = false;
 		}
-
 		
-		addSequential(new StraightAuto(timings[0]));
-		addSequential(new TurnAuto(false));
-		addSequential(new StraightAuto(timings[1]));
-		addSequential(new TurnAuto(true));
-		addSequential(new StraightAuto(timings[2]));
+		switch(target) {
+			case 'L': direction = true; break;
+			case 'R': direction = false; break;
+			default: direction = false; break;
+		}
+		
+		if (position == 'c') {
+
+			addSequential(new StraightAuto(timings[0]));
+			addSequential(new TurnAuto(false));
+			addSequential(new StraightAuto(timings[1]));
+			addSequential(new TurnAuto(direction));
+			addSequential(new StraightAuto(timings[2]));
+			
+		} else {
+			if (position == 'l' || position == 'r') {
+				if (position == Character.toLowerCase(target)) {
+					safeToPlace = true;
+				}
+				addSequential(new StraightAuto(length));
+				addSequential(new TurnAuto(!direction)); 
+				addSequential(new StraightAuto(timings[2]));
+			}
+		}
 		if (safeToPlace) addSequential(new PlaceCubeAuto());
 	}
 }
